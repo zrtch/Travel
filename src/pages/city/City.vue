10f71586751c6cd218fde07784a0fcc5e1aea683
zrtch <1160639597@qq.com>
2020-03-04 17:15:16 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,10 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hotCities="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hotCities="hotCities"
+    :letter="letter"
+    ></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -24,7 +26,8 @@ export default {
   data() {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -38,6 +41,10 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange(letter) {
+      this.letter = letter
+      console.log()
     }
   },
   mounted() {
