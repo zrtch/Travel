082e6cc3e16131ce2d69517807cff7996d3fe1f2
zrtch <1160639597@qq.com>
2020-03-04 20:15:47 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,62 @@
 <template>
+<div>
   <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
   </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
+</div>
+
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length
+    }
+  },
+  data() {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,4 +74,18 @@ export default {
         color:#666
         box-sizing :border-box
         padding:0 .1rem
+.search-content
+  position :absolute
+  overflow :hidden
+  z-index:1
+  top:1.58rem
+  right:0
+  left:0
+  bottom:0
+  background #ee
+  .search-item
+    line-height .62rem
+    padding .2rem
+    background #fff
+    color #666
 </style>
