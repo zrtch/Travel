cde8fb9b5235e49d18b47050dd84fd3d8e35e268
zrtch <1160639597@qq.com>
2020-03-05 21:48:34 +0800

    revise detail-header

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,16 +1,14 @@
 <template>
-<div>
-    <router-link tag="div" to="/" class="header-abs"
-    v-show="showAbs">
-        <div class="iconfont header-abs-back">&#xe624;</div>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
-        <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
-        <router-link to="/">
-            <div class="iconfont header-fixed-back">&#xe624;</div>
-      </router-link>
-        景点详情
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>景点详情
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -30,57 +28,63 @@ export default {
       if (top > 60) {
         let opacity = top / 140
         opacity = opacity > 1 ? 1 : opacity
-
-        this.opacityStyle = {
-          opacity
-        }
+        this.opacityStyle = { opacity }
         this.showAbs = false
       } else {
         this.showAbs = true
       }
     }
   },
-  activated() {
+  mounted() {
     window.addEventListener('scroll', this.handleScroll)
   },
-  deactivated() {
+  unmounted() {
     window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '../../../assets/styles/varibles'
+@import '.../../../../../assets/styles/varibles';
 
-  .header-abs
-      position absolute
-      left .2rem
-      top .2rem
-      width .8rem
-      height .8rem
-      border-radius .4rem
-      text-align center
-      line-height .8rem
-      background rgba(0,0,0,.8)
-      .header-abs-back
-          color #fff
-          font-size .4rem
-  .header-fixed
-      z-index 2
-      position: fixed
-      top 0
-      left 0
-      right 0
-      height :$headerHeight
-      line-height :$headerHeight
-      text-align :center
-      color:#fff
-      background :$bgColor
-      font-size:.32rem
-      .header-fixed-back
-          color #fff
-          position absolute
-          top 0
-          left 0
-          width .64rem
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+  width: 0.8rem;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  border-radius: 0.4rem;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.8);
+
+  .header-abs-back {
+    color: #fff;
+    font-size: 0.4rem;
+  }
+}
+
+.header-fixed {
+  z-index: 2;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: $headerHeight;
+  line-height: $headerHeight;
+  text-align: center;
+  color: #fff;
+  background: $bgColor;
+  font-size: 0.32rem;
+
+  .header-fixed-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0.64rem;
+    text-align: center;
+    font-size: 0.4rem;
+    color: #fff;
+  }
+}
 </style>
